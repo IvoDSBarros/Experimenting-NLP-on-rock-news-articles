efd330b36bad0ab7fb3783df33cc28d2a6edd171
Ivo Barros <142173353+IvoDSBarros@users.noreply.github.com>
2023-10-07 14:22:03 +0100

    Update rock_news_nlp_class_text_preprocessing.py

--- src/rock_news_nlp_class_text_preprocessing.py
+++ src/rock_news_nlp_class_text_preprocessing.py
@@ -55,8 +55,8 @@ class text_preprocessing:
         Args:
             text : str         
 		
-		Returns:
-			str
+	Returns:
+	    str
         """
         txt_temp = unidecode(self)
         txt_temp = re.sub(r'[^a-zA-Z0-9\s]+', ' ', txt_temp) 
@@ -70,8 +70,8 @@ class text_preprocessing:
             text : str
             additional_stop_words : set
 		
-		Returns:
-			str
+	Returns:
+	    str
         """
         words = word_tokenize(self)
         stop_words_extended = text_preprocessing.stop_words | text_preprocessing.additional_stop_words
@@ -80,14 +80,14 @@ class text_preprocessing:
     def word_search(word, self):
         """
         To search keywords across the text corpus        
-        Approach proposed on Stack Overflow (question: 28860440; user: user200783)
+        Approach proposed on Stack Overflow (question 5319922, user200783)
         
         Args:
             word : str
             text : str
 		
-		Returns:
-			bool
+	Returns:
+	    bool
         """
         return f' {word} ' in f' {self} '
           
@@ -99,8 +99,8 @@ class text_preprocessing:
             text : str
             dict_key_words : dict
 		
-		Returns:
-			str
+	Returns:
+	    str
         """      
         text_preprocessing.keyword_processor.add_keywords_from_dict(dict_keyword)
         return text_preprocessing.keyword_processor.replace_keywords(self)
@@ -114,8 +114,8 @@ class text_preprocessing:
             new_text : str
             words_to_remove : set
 		
-		Returns:
-			str
+	Returns:
+	    str
         """              
         pat = r'\b(?:{})\b'.format('|'.join(words_to_remove))
         return re.sub(pat, new_text, self)
@@ -128,8 +128,8 @@ class text_preprocessing:
         Args:
             text : str          
 		
-		Returns:
-			list of substrings			
+	Returns:
+	    list of substrings			
         """
         return word_tokenize(self)
     
@@ -143,8 +143,8 @@ class text_preprocessing:
             tokens : list
             set_keyword : set
 		
-		Returns:
-			list of substrings			
+	Returns:
+	    list of substrings			
         """
         tags = pos_tag(tokens)
         return [i for (i,j) in tags if ((j in ('NN','NNS') or j.startswith('V')) and i!='Bandname') or i in set_keyword]
@@ -157,8 +157,8 @@ class text_preprocessing:
         Args:
             tokens : list
 		
-		Returns:
-			list of substrings			
+	Returns:
+	    list of substrings			
         """
         stem_tokens = [text_preprocessing.ps.stem(word) for word in tokens if len(text_preprocessing.ps.stem(word))>2]
         return stem_tokens
@@ -171,8 +171,8 @@ class text_preprocessing:
         Args:
             tokens : list
 		
-		Returns:
-			list of substrings	
+	Returns:
+	    list of substrings	
         """
         bigram = Phrases(tokens, min_count=20, threshold=100)
         bigram_model = Phraser(bigram)
@@ -186,7 +186,7 @@ class text_preprocessing:
             lst : list         
     		
     	Returns:
-    		list
+    	    list
         """     
         lst_txt_temp = [i.lower() for i in lst]
         lst_txt_temp = [text_preprocessing.replace_keywords(i,text_preprocessing.dict_synonym_replacement) 
@@ -205,7 +205,7 @@ class text_preprocessing:
             lst : list         
     		
      	Returns:
-    		list
+    	    list
         """
         lst_txt_temp = text_preprocessing.text_preprocessing_prep(lst)
         lst_txt_temp = [text_preprocessing.get_noun_verb(i, text_preprocessing.set_keyword_lda) for i in  lst_txt_temp]
@@ -221,7 +221,7 @@ class text_preprocessing:
             lst : list         
     		
      	Returns:
-    		list
+    	    list
         """
         lst_txt_temp = text_preprocessing.text_preprocessing_prep(lst)
         lst_txt_temp = text_preprocessing.make_bigrams(lst_txt_temp) 
@@ -242,7 +242,7 @@ class text_preprocessing:
             lst : list         
     		
     	Returns:
-    		list
+    	    list
         """  
         lst_txt_temp = [i.lower() for i in lst]
         lst_txt_temp = [re.sub("’", "'", i) for i in lst_txt_temp]
@@ -257,7 +257,7 @@ class text_preprocessing:
             lst : list         
     		
     	Returns:
-    		list
+    	    list
         """
         lst_txt_temp = [text_preprocessing.remove_punctuation(i) for i in lst]
         lst_txt_temp = [text_preprocessing.remove_keywords(i,'Bandname',text_preprocessing.set_rock_artist_name) for i in lst_txt_temp]
